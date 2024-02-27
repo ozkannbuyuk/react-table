8448d0fdc670be6530a78105f792d3e009f949bb
Özkan Büyük <ozkanbuyuk@icloud.com>
2024-02-27 23:15:14 +0300

    React - Table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import Table from "./table";
 
 function App() {
+  const [users, setUsers] = useState(() => [
+    {
+      name: "Özkan",
+      surname: "Büyük",
+      email: "ozkanbuyuk@icloud.com",
+      age: 26,
+    },
+    {
+      name: "Ali Özkan",
+      surname: "Büyük",
+      email: "aliozkanbuyuk@icloud.com",
+      age: 26,
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-4">
+      <Table
+        searchable={true}
+        head={[
+          { name: "Name-Surname", sortable: true },
+          { name: "Email" },
+          { name: "Age", sortable: true },
+          { name: "Operations", width: 200 },
+        ]}
+        body={
+          users &&
+          users.map((user, key) => [
+            <div key={`${user.name} ${user.surname}`}>
+              {user.name} {user.surname}
+            </div>,
+            user.email,
+            <div searchableText={`Age ${user.age}`}>{user.age}</div>,
+            [
+              <button className="h-8 px-4 flex items-center justify-center rounded bg-blue-600 text-white">
+                Edit
+              </button>,
+              <button
+                onClick={() => {
+                  const tmpUsers = [...users];
+                  tmpUsers.splice(key, 1);
+                  setUsers(tmpUsers);
+                }}
+                className="h-8 px-4 flex items-center justify-center rounded bg-red-600 text-white"
+              >
+                Delete
+              </button>,
+            ],
+          ])
+        }
+      />
     </div>
   );
 }
